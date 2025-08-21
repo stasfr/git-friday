1a5285a0de074d399366ddc5779619fb5c5ac33a
stas_fr <sfworking@yandex.ru>
2025-08-21 16:01:16 +0300

    feat: implement sinceTag method to GitService
    Allows filtering commits from a specific tag onwards using `${tag}..`
    syntax

--- src/infrastructure/cli/services/git.service.ts
+++ src/infrastructure/cli/services/git.service.ts
@@ -60,6 +60,14 @@ export class GitService {
     return this;
   }
 
+  public sinceTag(tag: string | null | undefined): this {
+    if (tag) {
+      this.commandParts.push(`${tag}..`);
+    }
+
+    return this;
+  }
+
   public today(): this {
     this.commandParts.push('--since="00:00:00"');
 

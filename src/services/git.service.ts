0eb785b3ca585fd0807f69fd34a10d2c4b83975f
stas_fr <sfworking@yandex.ru>
2026-01-30 00:17:02 +0300

    feat(git): Add forRange and sinceTag methods to GitService

--- src/services/git.service.ts
+++ src/services/git.service.ts
@@ -72,6 +72,16 @@ export class GitService {
     return this;
   }
 
+  public forRange(range: string) {
+    this.commandParts.push(range);
+    return this;
+  }
+
+  public sinceTag(tag: string) {
+    this.commandParts.push(`${tag}..`);
+    return this;
+  }
+
   public async getCommitLog() {
     const command = this.commandParts.join(' ');
     const cwd = process.cwd();

b54438848d9a89af07e63a554c0de670a6d6f85f
stas_fr <sfworking@yandex.ru>
2026-02-12 23:15:06 +0300

    refactor(cli): move cli init to related file in cli folder

---
+++ src/cli/runCli.ts
@@ -0,0 +1,30 @@
+import pkg from '../../package.json' with { type: 'json' };
+
+import { Command } from 'commander';
+
+import { config } from '@/cli/config/configCommand.js';
+import { changelog } from '@/cli/changelog/changelogCommand.js';
+import { pr } from '@/cli/pr/prCommand.js';
+import { report } from '@/cli/report/reportCommand.js';
+
+export async function runCli() {
+  try {
+    const program = new Command();
+
+    program.name('friday').version(pkg.version).description(pkg.description);
+
+    config(program);
+
+    changelog(program);
+    pr(program);
+    report(program);
+
+    await program.parseAsync(process.argv);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.log('Error: ', error.message);
+    } else {
+      console.log('Unknown error: ', error);
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,15 @@
 #!/usr/bin/env node
 
 import 'dotenv/config';
-import pkg from '../package.json' with { type: 'json' };
 
 import { setupLocalization } from '@/localization/localization.js';
 import { checkForUpdates } from '@/services/pkgUpdateService.js';
-import { Command } from 'commander';
-
-import { config } from '@/cli/config/configCommand.js';
-
-import { changelog } from '@/cli/changelog/changelogCommand.js';
-import { pr } from '@/cli/pr/prCommand.js';
-import { report } from '@/cli/report/reportCommand.js';
+import { runCli } from '@/cli/runCli.js';
 
 async function main() {
   await setupLocalization();
-
   await checkForUpdates();
-
-  try {
-    const program = new Command();
-
-    program.name('friday').version(pkg.version).description(pkg.description);
-
-    config(program);
-
-    changelog(program);
-    pr(program);
-    report(program);
-
-    await program.parseAsync(process.argv);
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log('Error: ', error.message);
-    } else {
-      console.log('Unknown error: ', error);
-    }
-  }
+  await runCli();
 }
 
 void main();

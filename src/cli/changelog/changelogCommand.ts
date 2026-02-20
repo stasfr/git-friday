0c90f667f40eaa460d15bd0646ce57c0074922ee
stas_fr <sfworking@yandex.ru>
2026-02-21 02:35:31 +0300

    refactor(config): Remove configExistCheckHook

--- src/cli/changelog/changelogCommand.ts
+++ src/cli/changelog/changelogCommand.ts
@@ -1,13 +1,11 @@
 import { Command } from 'commander';
 import { ConfigService } from '@/cli/config/configService.js';
-import { configExistCheckHook } from '@/cli/config/hooks/configExistCheckHook.js';
 import { changelogAction } from '@/cli/changelog/changelogAction.js';
 
 export function changelog(program: Command) {
   program
     .command('changelog')
     .description('Generate a changelog from git commits since a specific tag')
-    .hook('preAction', configExistCheckHook)
     .action(async () => {
       const configService = new ConfigService();
       const appConfig = await configService.getAppConfig();
--- src/cli/pr/prCommand.ts
+++ src/cli/pr/prCommand.ts
@@ -1,6 +1,5 @@
 import { Command } from 'commander';
 import { ConfigService } from '@/cli/config/configService.js';
-import { configExistCheckHook } from '@/cli/config/hooks/configExistCheckHook.js';
 import { prAction } from '@/cli/pr/prAction.js';
 
 export function pr(program: Command) {
@@ -9,7 +8,6 @@ export function pr(program: Command) {
     .description(
       'Generate a PR report based on commit history within pull requests',
     )
-    .hook('preAction', configExistCheckHook)
     .action(async () => {
       const configService = new ConfigService();
       const appConfig = await configService.getAppConfig();

b29ff773f5b41a42c594253a7c8f692de530cedd
stas_fr <sfworking@yandex.ru>
2026-02-21 00:47:22 +0300

    refactor(report): refactored reportAction and improve error handling
    - Replace boxen with standard console logs for simplicity
    - Implement ExtendedError for more detailed error reporting
    - Move input prompt outside of the main logic block
    - Simplify console output formatting and spacing

--- src/cli/report/reportAction.ts
+++ src/cli/report/reportAction.ts
@@ -1,7 +1,7 @@
 import ora from 'ora';
-import boxen from 'boxen';
 import { input } from '@inquirer/prompts';
 
+import { ExtendedError } from '@/errors/ExtendedError.js';
 import { LlmService } from '@/services/llmService.js';
 import { GitService } from '@/services/gitService.js';
 import { reportPrompts } from '@/cli/report/reportPrompts.js';
@@ -11,82 +11,72 @@ import { generateUsageTables } from '@/helpers/generateUsageTables.js';
 import type { AppConfig } from '@/cli/config/configTypes.js';
 
 export async function reportAction(appConfig: AppConfig) {
-  const spinner = ora();
+  const spinner = ora({ color: 'green' });
   const gitService = new GitService();
   const llmService = new LlmService(appConfig);
 
-  try {
-    const customLog = await input({
-      message: 'Enter your custom git log command: git log',
+  console.log('Enter your custom git log command:');
+  const customLog = await input({
+    message: 'git log',
+  });
+
+  const sourceCommits = await gitService
+    .customLog(customLog)
+    .pretty()
+    .getCommitLog();
+
+  if (sourceCommits.length === 0) {
+    throw new ExtendedError({
+      layer: 'CommandExecutionError',
+      message: 'No commits found for the specified criteria',
+      command: 'report',
+      service: null,
+      hint: 'Check your git log command and try again',
     });
+  }
+  console.log(`\nCommits count: ${sourceCommits.length}\n`);
 
-    const sourceCommits = await gitService
-      .customLog(customLog)
-      .pretty()
-      .getCommitLog();
+  const systemPrompt = reportPrompts.getSystemPrompts(
+    appConfig.llmPromptsLocalization,
+  );
+  const userPrompt = reportPrompts.getUserPrompt(
+    sourceCommits.join('\n'),
+    appConfig.llmPromptsLocalization,
+  );
 
-    if (sourceCommits.length === 0) {
-      throw new Error('No commits found for the specified criteria');
-    }
+  spinner.start('Generating report...');
 
-    console.log(
-      boxen(
-        `Command: ${gitService.command}\nCommit Count: ${sourceCommits.length}`,
-        {
-          title: 'Git Log',
-          padding: 0.75,
-          margin: 0.75,
-          borderColor: 'green',
-          borderStyle: 'round',
-        },
-      ),
-    );
+  const llmResponse = await llmService.getCompletion({
+    systemPrompt,
+    userPrompt,
+  });
 
-    spinner.start('Generating report...');
-
-    const systemPrompt = reportPrompts.getSystemPrompts(
-      appConfig.llmPromptsLocalization,
-    );
-    const userPrompt = reportPrompts.getUserPrompt(
-      sourceCommits.join('\n'),
-      appConfig.llmPromptsLocalization,
-    );
-
-    const llmResponse = await llmService.getCompletion({
-      systemPrompt,
-      userPrompt,
+  if (!llmResponse) {
+    throw new ExtendedError({
+      layer: 'CommandExecutionError',
+      message: 'Got empty response from Llm Provider',
+      command: 'report',
+      service: null,
+      hint: 'Check LLM provider key and url and try again',
     });
+  }
 
-    if (!llmResponse) {
-      throw new Error('Got empty response from Llm Provider');
+  spinner.succeed('Report generated successfully');
+
+  console.log('\nReport:');
+  console.log(llmResponse.content.trim());
+
+  if (llmResponse.usage) {
+    const usageTables = generateUsageTables(llmResponse.usage);
+
+    if (usageTables.tokens) {
+      console.log('\nTokens Usage Statistics:');
+      console.table(usageTables.tokens);
     }
 
-    spinner.succeed('Report generated successfully');
-
-    console.log();
-    console.log('Report:');
-    console.log(llmResponse.content.trim());
-
-    if (llmResponse.usage) {
-      const usageTables = generateUsageTables(llmResponse.usage);
-
-      if (usageTables.tokens) {
-        console.log();
-        console.log('Tokens Usage Statistics:');
-        console.table(usageTables.tokens);
-      }
-
-      if (usageTables.cost) {
-        console.log();
-        console.log('Cost Statistics in $');
-        console.table(usageTables.cost);
-      }
+    if (usageTables.cost) {
+      console.log('\nCost Statistics in $');
+      console.table(usageTables.cost);
     }
-  } catch (error: unknown) {
-    spinner.fail(
-      `Error occurred: ${
-        error instanceof Error ? error.message : String(error)
-      }`,
-    );
   }
 }

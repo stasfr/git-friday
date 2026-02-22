051a3f7db6d87261f14b4dc144bf02a314151d9b
stas_fr <sfworking@yandex.ru>
2026-02-23 02:55:46 +0300

    fix: update help messages and commands order

--- src/cli/profile/profileCommand.ts
+++ src/cli/profile/profileCommand.ts
@@ -1,21 +1,21 @@
 import { Command } from 'commander';
 
-import { useProfileConfigCommand } from '@/cli/profile/config/profileConfigCommand.js';
+import { useProfileListCommand } from '@/cli/profile/list/profileListCommand.js';
 import { useProfileCreateCommand } from '@/cli/profile/create/profileCreateCommand.js';
 import { useProfileSetupCommand } from '@/cli/profile/setup/profileSetupCommand.js';
-import { useProfileListCommand } from '@/cli/profile/list/profileListCommand.js';
 import { useProfilePromptCommand } from '@/cli/profile/prompt/profilePromptCommand.js';
+import { useProfileConfigCommand } from '@/cli/profile/config/profileConfigCommand.js';
 import { useProfileDeleteCommand } from '@/cli/profile/delete/profileDeleteCommand.js';
 
 export function useProfileCommand(program: Command) {
   const profileCommand = program
     .command('profile')
-    .description('Manage and execute user profiles');
+    .description('Manage and user profiles');
 
-  useProfileConfigCommand(profileCommand);
+  useProfileListCommand(profileCommand);
   useProfileCreateCommand(profileCommand);
   useProfileSetupCommand(profileCommand);
-  useProfileListCommand(profileCommand);
   useProfilePromptCommand(profileCommand);
+  useProfileConfigCommand(profileCommand);
   useProfileDeleteCommand(profileCommand);
 }

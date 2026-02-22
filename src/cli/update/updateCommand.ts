5492225cef59fcb74671ade2f37aab748810ec90
stas_fr <sfworking@yandex.ru>
2026-02-23 02:50:14 +0300

    fix(update): fix description for root command

--- src/cli/update/updateCommand.ts
+++ src/cli/update/updateCommand.ts
@@ -3,9 +3,7 @@ import { Command } from 'commander';
 import { useUpdateCheckCommand } from '@/cli/update/check/updateCheckCommand.js';
 
 export function useUpdateCommand(command: Command) {
-  const updateCommand = command
-    .command('update')
-    .description('Check for updates');
+  const updateCommand = command.command('update').description('Manage updates');
 
   useUpdateCheckCommand(updateCommand);
 }
